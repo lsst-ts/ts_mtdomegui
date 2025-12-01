18e5656ba1da8f5a6f082767504a35536207a2e5
Te-Wei <twtsaia@gmail.com>
2025-12-01 14:29:37 -0700

    Add the documenteer.toml.

--- doc/conf.py
+++ doc/conf.py
@@ -4,13 +4,17 @@ This configuration only affects single-package Sphinx documentation builds.
 """
 
 import lsst.ts.mtdomegui  # type: ignore # noqa
-from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
+from documenteer.conf.guide import *  # type: ignore # noqa
 
 project = "ts_mtdomegui"
 html_theme_options["logotext"] = project  # type: ignore # noqa
 html_title = project
 html_short_title = project
 doxylink = {}  # type: ignore # noqa
+
+intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # type: ignore # noqa
+intersphinx_mapping["ts_guitool"] = ("https://ts-guitool.lsst.io", None)  # type: ignore # noqa
+intersphinx_mapping["ts_mtdomecom"] = ("https://ts-mtdomecom.lsst.io", None)  # type: ignore # noqa
 
 # Support the sphinx extension of mermaid
 extensions = [
